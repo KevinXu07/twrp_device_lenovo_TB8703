4be3861c42c0fc904beae6b8658a501e9c61bd42
Kevin <111238244+KevinXu07@users.noreply.github.com>
2023-10-25 10:50:03 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-COMMON_LUNCH_CHOICES omni_TB8703X-user
-COMMON_LUNCH_CHOICES omni_TB8703X-userdebug
-COMMON_LUNCH_CHOICES omni_TB8703X-eng
+COMMON_LUNCH_CHOICES := \
+    omni_TB8703X-user \
+    omni_TB8703X-userdebug \
+    omni_TB8703X-eng
